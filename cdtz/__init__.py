52383953c2e7beb2bf22a19c5caef4ca85645139
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-10-14 12:54:40 +0300

    Add Denis Gavrilyuk to copyright

--- cdtz/__init__.py
+++ cdtz/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2020 Vladislav Yarovoy. All Rights Reserved.
+# Copyright (C) 2020 Denis Gavrilyuk. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
